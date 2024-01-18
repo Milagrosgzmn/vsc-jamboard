a9d651b1a704efe3bdaa55978df2675aa3e480b4
milagros <miliguzman0@gmail.com>
2024-01-17 22:35:17 -0300

    add: state persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor} >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
