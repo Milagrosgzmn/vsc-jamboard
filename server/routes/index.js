a6afac4645385aa2cd33520cdc156398dd2ed5f2
milagros <miliguzman0@gmail.com>
2024-02-14 00:32:06 -0300

    add: contact routes and post board route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,32 @@ const {Router} = require('express');
 const login = require('../controllers/usersControllers/login');
 const signUp = require('../controllers/usersControllers/signUp');
 const logOut = require('../controllers/usersControllers/logOut');
+const sendInvitation = require('../controllers/usersControllers/sendInvitation');
+const getAllContacts = require('../controllers/usersControllers/getAllContacts');
+const addContact = require('../controllers/usersControllers/addContact');
+
 const mainRouter = Router();
 
-//handlers require
+const requireAuth = require('../middlewares/authenticationToken');
+const deleteContact = require('../controllers/usersControllers/deleteContact');
+const postBoard = require('../controllers/boardControllers/postBoard');
+
 
 //rutas
+
+// User
 mainRouter.post('/login', login);
 mainRouter.post('/signUp', signUp);
 mainRouter.get('/logOut', logOut);
+
+// relacionado a contactos
+mainRouter.post('/contact/check', requireAuth, sendInvitation); // o deberia ser un get?
+mainRouter.get('/contact/:user_id',requireAuth, getAllContacts);
+mainRouter.delete('/contact/:user_id', deleteContact);
+mainRouter.post('/contact/add', addContact)
+
+// boards
+mainRouter.post('/board/new', postBoard)
 
 module.exports = mainRouter;
 
