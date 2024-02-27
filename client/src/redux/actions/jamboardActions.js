bcc10499147d74e9f6d1058c0c570ac736a7621d
milagros <miliguzman0@gmail.com>
2024-02-26 23:27:38 -0300

    change: set boards from the backend

--- client/src/redux/actions/jamboardActions.js
+++ client/src/redux/actions/jamboardActions.js
@@ -1,7 +1,24 @@
 import { setBoards } from "../slices/jamboardSlice";
+import Swal from "sweetalert2";
+import axios from "axios";
 
-export const setMyBoards = (boards)=>{
-    return (dispatch)=>{
-        dispatch(setBoards(boards));
+export const setMyBoards = (user_id)=>{
+    const endpoint = `/board/${user_id}`
+    return async (dispatch)=>{
+        try {
+            const {data} = await axios(endpoint, {
+                withCredentials: true,
+              });
+            
+            dispatch(setBoards(data))
+
+        } catch (error) {
+             console.error(error.message);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Hubo un error.',
+          })
+        }
     }
-};
+}
