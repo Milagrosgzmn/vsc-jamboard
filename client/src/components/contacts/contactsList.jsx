b5c265360e051a98a33c03e48d18727ca420cd56
milagros <miliguzman0@gmail.com>
2024-03-20 20:42:45 -0300

    add: search bar function

--- client/src/components/contacts/contactsList.jsx
+++ client/src/components/contacts/contactsList.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import { useEffect, useState } from "react";
 import Contact from "./contact"
 
@@ -7,17 +9,31 @@ import profile1 from '../../assets/images/profile1.jpg'
 import profile2 from '../../assets/images/profile2.jpg'
 import profile3 from '../../assets/images/profile3.jpg'
 import profile4 from '../../assets/images/profile4.jpg'
+import AddContact from "./addContact";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { contactsReset, filterContacts} from '../../redux/actions/userActions';
 
 export default function ContactsList() {
 
-    const contacts = useSelector(state=>{
-        return state.user.contacts;
+    const dispatch = useDispatch();
+    const {contacts} = useSelector(state=>{
+        return state.user;
     })
+    useEffect(()=>{
+
+    },[contacts])
      
     const profilePics = [profile1,profile2,profile3,profile4];
-
+    
+    const [isSearching, setSearching] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const [addContact, setAddContact] = useState(false)
+    const [contactList, setContactList] = useState([]);
+    const [buttonCont] = useState({
+        small:'+',
+        normal:'Nuevo contacto +'
+    })
     const [visibleContacts, setVisibleContacts] = useState(4)
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(visibleContacts);
@@ -59,18 +75,59 @@ export default function ContactsList() {
         setStart((prevStart) => Math.max(prevStart - visibleContacts, 0));
         setEnd((prevEnd) => Math.max(prevEnd - visibleContacts, visibleContacts));
     }
+    function handleChangeAlreadyContact(e){
+        if (e.target.value) {
+            setSearching(true)
+            const value = e.target.value.toLowerCase();
+        dispatch(filterContacts(value))
+        }else{
+            setSearching(false)
+            dispatch(contactsReset())
+        }
+        
+     }
+    const handleSearchContact = async(e)=>{
+       try {
+        setSearchValue(e.target.value);
+        let searchV = searchValue.trim();
+
+        const {data} = await axios(`/contact?username=${searchV}`);
+            setContactList(data)
+            console.log(data);
+       } catch (error) {
+        console.error(error);
+       }
+
+
+    }
 
     return(
         <section className="w-10/12 flex flex-col my-16">
-            <div className="flex flex-row justify-between items-center">
-                <h2>Mi red</h2>
-                <div className="flex">
-                    <button className="bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl"> Nuevo contacto +</button>
-                    <input className="rounded-2xl px-4 py-1 border-2 border-inheret" type="search" placeholder="Buscar contacto"/>
+            <div className="flex sm:flex-row flex-col  justify-between items-center">
+                <h2 className="font-bold sm:text-3xl text-xl sm:w-auto w-full">Mi red</h2>
+                <div className="flex md:text-base text-xs mt-4 sm:mt-1">
+                    {addContact && 
+                    <input value={searchValue} onChange={handleSearchContact} className="rounded-2xl px-4 py-1 border-2 border-inheret" type="search" placeholder="Ingresa el nombre de usuario"/>
+                    }
+                    <div className="h-max">
+                        { contactList.length ? <AddContact/> : ''}
+                    </div>
+                    <button onClick={()=>{
+                        setAddContact(!addContact)
+                    }}className="sm:block hidden bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl">{addContact ? 'x' : buttonCont.normal}</button>
+                    <button onClick={()=>{
+                        setAddContact(!addContact)
+                    }}
+                    className="sm:hidden bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl">{addContact ? 'x' : buttonCont.small}</button>
+                    
+                    <input className="rounded-2xl px-4 py-1 border-2 border-inheret mx-1" type="search" placeholder="Buscar contacto" onChange={handleChangeAlreadyContact}/>
+                   {isSearching && <button className="bg-neutral-800 rounded-2xl px-4 py-1 text-white font-semibold " onClick={()=>{
+                        dispatch(contactsReset())
+                    }}>Reset</button>}
                 </div>
             </div>
 
-            <div className="relative flex items-center">
+            <div className="relative flex items-center mt-4">
                 <div className="absolute left-0 rounded-full bg-black/20 cursor-pointer" onClick={handlePrev}>
                     <ArrowLeftRoundedIcon/>
                 </div>
@@ -78,7 +135,7 @@ export default function ContactsList() {
                     <ArrowRightRoundedIcon/>
                 </div> 
                 <div className=" mx-auto w-11/12">
-                    <article className="w-full h-40 flex items-center">
+                    <article className="w-full h-40 flex items-center justify-center">
                         
                         {contacts.slice(start, end).map((contact, i) =>(
                             <Contact key={i} i={i} pics={profilePics} contact={contact} />
--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -4,12 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { logOutUser, setMyContacts } from "../redux/actions/userActions";
 import ContactsList from "../components/contacts/contactsList";
 import ProjectGrid from "../components/projects/projectGrid";
-import { setMyBoards } from "../redux/actions/jamboardActions";
+import { boardsReset, filterBoards, setMyBoards } from "../redux/actions/jamboardActions";
 import ProjectForm from "../components/projectForm/projectForm";
 import ContactForm from "../components/contacts/contactForm";
 
 export default function Home() {
-
+  const [isSearching, setSearching] = useState(false)
     const [openContactMenu, setContactMenu] = useState(false)
     const [openProjectForm, setOpenProjectForm] = useState(false);
     const dispatch = useDispatch();
@@ -42,6 +42,19 @@ export default function Home() {
       }
       
     },[])
+
+    function handleChangeProjects(e){
+      if (e.target.value) {
+          setSearching(true)
+          const value = e.target.value.toLowerCase();
+      dispatch(filterBoards(value))
+      }else{
+          setSearching(false)
+          dispatch(boardsReset())
+      }
+      
+   }
+
     return (
       < main className={openProjectForm ? 'flex flex-col pt-16 items-center relative' : 'flex flex-col pt-16 items-center' }>
         <ContactsList/>
@@ -52,7 +65,12 @@ export default function Home() {
             <div className="flex md:text-base text-xs mt-4 sm:mt-1">
               <button onClick={()=>{setOpenProjectForm(true)}} className="sm:block hidden bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl"> Nuevo proyecto +</button>
               <button onClick={()=>{setOpenProjectForm(true)}} className="sm:hidden bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl"> +</button>
-              <input className=" rounded-2xl px-4 py-1 border-2 border-inheret" type="search" placeholder="Buscar proyecto"/>
+              <input className=" rounded-2xl px-4 py-1 border-2 border-inheret" 
+                onChange={handleChangeProjects}
+                type="search" placeholder="Buscar proyecto"/>
+              {isSearching && <button className="bg-neutral-800 rounded-2xl px-4 py-1 text-white font-semibold " onClick={()=>{
+                        dispatch(boardsReset())
+                    }}>Reset</button>}
             </div>
           </div>
           
