0924bfa3bb4a8a2d850333d3fe6da180c61fa0ce
milagros <miliguzman0@gmail.com>
2024-03-20 11:45:59 -0300

    add: styles for project grid and setOpenMenu for contacts

--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -6,9 +6,11 @@ import ContactsList from "../components/contacts/contactsList";
 import ProjectGrid from "../components/projects/projectGrid";
 import { setMyBoards } from "../redux/actions/jamboardActions";
 import ProjectForm from "../components/projectForm/projectForm";
+import ContactForm from "../components/contacts/contactForm";
 
 export default function Home() {
-  
+
+    const [openContactMenu, setContactMenu] = useState(false)
     const [openProjectForm, setOpenProjectForm] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -45,18 +47,20 @@ export default function Home() {
         <ContactsList/>
         <section className="w-10/12">
           
-          <div className="flex flex-row justify-between items-center">
-            <h2> Mis proyectos</h2>
-            <div className="flex">
-              <button onClick={()=>{setOpenProjectForm(true)}} className="bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl"> Nuevo proyecto +</button>
-              
-              <input className="rounded-2xl px-4 py-1 border-2 border-inheret" type="search" placeholder="Buscar proyecto"/>
+          <div className="flex sm:flex-row flex-col justify-between items-center">
+            <h2 className="font-bold sm:text-3xl text-xl sm:w-auto w-full"> Mis proyectos</h2>
+            <div className="flex md:text-base text-xs mt-4 sm:mt-1">
+              <button onClick={()=>{setOpenProjectForm(true)}} className="sm:block hidden bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl"> Nuevo proyecto +</button>
+              <button onClick={()=>{setOpenProjectForm(true)}} className="sm:hidden bg-green-500 font-semibold mx-2 py-2 px-4 rounded-xl"> +</button>
+              <input className=" rounded-2xl px-4 py-1 border-2 border-inheret" type="search" placeholder="Buscar proyecto"/>
             </div>
           </div>
-
-          <ProjectGrid/>
+          
+            <ProjectGrid setOpen={setOpenProjectForm} setContactMenu={setContactMenu} />
+          
         </section>
         {openProjectForm && <ProjectForm setOpen={setOpenProjectForm}/>}
+        {openContactMenu && <ContactForm setContactMenu={setContactMenu}/>} 
       </main>
     )
 }
