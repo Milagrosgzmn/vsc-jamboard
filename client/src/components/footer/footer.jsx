ca34cbf2a2ecf9aa49823eaebdfb15371fd31907
milagros <miliguzman0@gmail.com>
2024-04-01 22:30:30 -0300

    add: footer

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+
+    return (
+        <footer className="w-full relative bottom-0 mt-8 darkbg">
+            <article className="text-center" >
+                <p className="font-semibold py-2"> 2024 |  milagros.guzmanabarca@gmail.com</p>
+            </article>        
+        </footer>
+    )
+}
