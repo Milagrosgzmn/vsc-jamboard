0ab8cb1023163524612bc5391b04c68a5e605788
milagros <miliguzman0@gmail.com>
2024-04-01 23:36:31 -0300

    change: delete contact and added swal for redux action

--- client/src/components/optionsMenu/contactOptionsMenu.jsx
+++ client/src/components/optionsMenu/contactOptionsMenu.jsx
@@ -1,9 +1,21 @@
-export default function ContactOptionsMenu() {
+/* eslint-disable react/prop-types */
+
+import { useDispatch, useSelector } from "react-redux";
+import { removeContact } from "../../redux/actions/userActions";
+export default function ContactOptionsMenu({contact}) {
+    const dispatch = useDispatch();
+    const {id} = useSelector(state=>{
+        return state.user;
+    });
 
     return(
         <article className=' absolute left-full top-0 bg-neutral-900 text-white w-32 h-max rounded-2xl hover:bg-neutral-700'>
                 <ul className='py-2 font-medium text-xs'>
-                    <li className='w-full px-4 py-2 hover:text-red-600 cursor-pointer'>Eliminar contacto</li>
+                    <li className='w-full px-4 py-2 hover:text-red-600 cursor-pointer'
+                        onClick={()=>{
+                            dispatch(removeContact(id, contact.id))
+                        }}
+                    >Eliminar contacto</li>
                 </ul>
 
         </article>
