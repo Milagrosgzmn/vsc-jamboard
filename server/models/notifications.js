27e443160faee312a1bce6964f9179f261965ccf
milagros <miliguzman0@gmail.com>
2024-05-20 22:45:50 -0300

    change: notifications model

--- server/models/notifications.js
+++ server/models/notifications.js
@@ -1,17 +1,51 @@
 const { DataTypes} = require('sequelize')
+const { Users } = require('../DB_connection')
 
 module.exports = (sequelize)=>{
     const  Notifications = sequelize.define('Notifications',{
         issue: {
             type:DataTypes.STRING,
             allowNull: false,
+        },
+        isRead:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        senderId: {
+            type: DataTypes.UUID,
+            references: {
+                model: Users,
+                key: 'id'
+            }
+        },
+        receiverId: {
+            type: DataTypes.UUID,
+            references: {
+                model: Users,
+                key: 'id'
+            }
         }
-    
     },
     {
       timestamps:false,
     }
     )  
+    Notifications.sentFriendReq = async function(user_id, friend_id){
+
+        try {
+            await this.create({
+                issue: 'connectRequest',
+                isRead: false,
+                senderId: user_id,
+                receiverId: friend_id
+            });
+            return true;
+        } catch (error) {
+            console.error('Error creating notification:', error);
+        }
+
+
+    }
 
     return Notifications;
 }
